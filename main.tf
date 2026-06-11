5af688850e28ba83e12c908a4096620bba3cde8d
Hollztothewalls <144404653+Hollztothewalls@users.noreply.github.com>
2026-06-11 15:07:15 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,7 @@
 data "aws_ami" "app_ami" {
   most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-  }
+
 
   filter {
     name   = "virtualization-type"
